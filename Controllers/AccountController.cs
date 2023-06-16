7476c0e6c7f5253494d2b7759ca27fba059b702b
Taislaine Massucato <massucato2@outlook.com>
2023-06-16 13:03:58 -0300

    Authenticação e autorização acesso ao Token

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,11 +1,13 @@
 ﻿using Blog.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers
-{
+{   
+  
     [ApiController]
     public class AccountController : ControllerBase
-    {
+    { 
 
         [HttpPost("v1/login")]
         public IActionResult Login([FromServices] TokenService tokenService)
@@ -15,5 +17,17 @@ namespace Blog.Controllers
 
             return Ok(token);
         }
+
+        [Authorize(Roles = "user")]
+        [HttpGet("v1/user")]
+        public IActionResult GetUser() => Ok(User.Identity.Name);
+
+        [Authorize(Roles = "author")] 
+        [HttpGet("v1/author")]
+        public IActionResult GetAuthor() => Ok(User.Identity.Name);
+
+        [Authorize(Roles = "admin")]
+        [HttpGet("v1/admin")]
+        public IActionResult GetAdmin() => Ok(User.Identity.Name);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,28 @@
+using Blog;
 using Blog.Data;
 using Blog.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 
 builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
 {
@@ -16,6 +37,9 @@ builder.Services.AddTransient<TokenService>(); //sempre cria novo
 
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
